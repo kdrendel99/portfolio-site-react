9b9faa3b6645fbe0db5bfef22efac62892b8f32a
Karlson Drendel <kdrendel99@gmail.com>
2021-09-14 22:07:37 -0700

    Seed data file.

--- src/components/AllProjects.js
+++ src/components/AllProjects.js
@@ -12,6 +12,16 @@ const AllProjects = [
     id: v4()
   },
   {
+    name: "National Parks API",
+    image: "national-park.jpg",
+    imgDes: "Yosemite clip art",
+    category: "Web API",
+    projDate: "10 July, 2021",
+    githubUrl: "https://github.com/kdrendel99/National-Parks-API",
+    description: "This project is National Parks API. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    id: v4()
+  },
+  {
     name: "High School Jukebox",
     image: "jukebox.jpg",
     imgDes: "A jukebox.",
@@ -21,38 +31,36 @@ const AllProjects = [
     description: "This project is High School Jukebox. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     id: v4()
   },
-  // {
-  //   name: "Satoshi's Crypto",
-  //   image: 24.99,
-  //   category: "The coolest, most rad tee shirt you could ever want.",
-  //   projDate: 100,
-  //   githubUrl: SVGFEDisplacementMapElement,
-  //   description: 9
-  // },
-  // {
-  //   name: "Satoshi's Crypto",
-  //   image: 24.99,
-  //   category: "The coolest, most rad tee shirt you could ever want.",
-  //   projDate: 100,
-  //   githubUrl: SVGFEDisplacementMapElement,
-  //   description: 9
-  // },
-  // {
-  //   name: "Satoshi's Crypto",
-  //   image: 24.99,
-  //   category: "The coolest, most rad tee shirt you could ever want.",
-  //   projDate: 100,
-  //   githubUrl: SVGFEDisplacementMapElement,
-  //   description: 9
-  // },
-  // {
-  //   name: "Satoshi's Crypto",
-  //   image: 24.99,
-  //   category: "The coolest, most rad tee shirt you could ever want.",
-  //   projDate: 100,
-  //   githubUrl: SVGFEDisplacementMapElement,
-  //   description: 9
-  // } 
+  {
+    name: "Factory Manager",
+    image: "factory.jpg",
+    imgDes: "A factory.",
+    category: "Web Application",
+    projDate: "13 June, 2021",
+    githubUrl: "https://github.com/kdrendel99/Factory-Solution",
+    description: "This project is Factory Manager. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    id: v4()
+  },
+  {
+    name: "Taproom Manager",
+    image: "taproom.png",
+    imgDes: "Beer glasses.",
+    category: "Web Application",
+    projDate: "29 August, 2021",
+    githubUrl: "https://github.com/kdrendel99/Tap-Room-Manager---React-App",
+    description: "This project is Taproom Manager. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    id: v4()
+  },
+  {
+    name: "Exchange Rate Calculator",
+    image: "exchange.jpg",
+    imgDes: "Coins.",
+    category: "Web Application",
+    projDate: "7 April 2021",
+    githubUrl: "https://github.com/kdrendel99/exchange-rate-calculator",
+    description: "This project is Exchange Rate Calculator. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    id: v4()
+  },
 ];
 
 export default AllProjects;

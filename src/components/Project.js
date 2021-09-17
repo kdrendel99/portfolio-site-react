e6a4f24135257c93189c1c34c41659d51c75b8ae
Karlson Drendel <kdrendel99@gmail.com>
2021-09-17 16:42:38 -0700

    Add mapping functionality to dry up Home projects section.

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,53 +1,71 @@
-// import React from "react";
-// import PropTypes from "prop-types";
-// // import './../img/portfolio/'
-// // import rocket from './../img/portfolio/rocket.jpg';
-// // import park from './../img/portfolio/nationalpark.jpg';
-// // import jukebox from './../img/portfolio/jukebox.jpg';
-// // import factory from './../img/portfolio/factory.jpg';
-// // import taproom from './../img/portfolio/taproom.png';
-// // import exchange from './../img/portfolio/exchange.jpg';
-// // './../img/portfolio'
-// // ={require(`./../img/portfolio/${props.image}`)}
+import React, {useEffect} from "react";
+import PropTypes from "prop-types";
+import rocket from './../img/portfolio/rocket.jpg';
+import park from './../img/portfolio/nationalpark.jpg';
+import jukebox from './../img/portfolio/jukebox.jpg';
+import factory from './../img/portfolio/factory.jpg';
+import taproom from './../img/portfolio/taproom.png';
+import exchange from './../img/portfolio/exchange.jpg';
+import './../../node_modules/swiper/swiper-bundle.min.js';
 
-// function Project(props){
-//   return (
+function Project(props){
+    const [img, setImg] = React.useState(null);
 
-//     <React.Fragment>
-//       <div onClick = {() => props.whenProjClicked(props.id)}>
+    function renderImage(){
+      let incomingImg = props.image;
+  
+      switch (incomingImg) {
+        case 'rocket':
+          setImg(rocket);
+          break;
+        case 'park':
+          setImg(park);
+          break;
+        case 'jukebox':
+          setImg(jukebox);
+          break;
+        case 'factory':
+          setImg(factory);
+          break;
+        case 'taproom':
+          setImg(taproom);
+          break;
+        case 'exchange':
+          setImg(exchange);
+          break;
+        default:
+          setImg(null);
+      }
+    }
+  
+    useEffect(() => {
+      renderImage();
+    })
+  return (
 
-//           {/* <div className="col-lg-4 col-md-6 portfolio-item filter-app"> */}
-//             <img src={require(`./../img/portfolio/${props.image}`)} alt={`${props.imgDes}`}/>
-//             {/* <div className="portfolio-info">
-//               <h4>{props.name}</h4>
-//               <p>{props.description}</p> */}
-//               {/* <a href={require(`./../img/portfolio/${props.image}`)}data-gallery="portfolioGallery" className="portfolio-lightbox preview-link" title="Web 2"><i className="bx bx-plus"></i></a> */}
-//               {/* <a className="details-link" title="More Details"><i className="bx bx-link"></i></a>
-//             </div>
-//           </div> */}
+    <React.Fragment>
+          <div className="col-lg-4 col-md-6 portfolio-item filter-app">
+            <img src={img} className="img-fluid" alt=""/>
+            <div className="portfolio-info">
+              <h4>{props.name}</h4>
+              <p>{props.description}</p>
+              <a href={img} data-gallery="portfolioGallery" className="portfolio-lightbox preview-link" title={props.type}><i className="bx bx-plus"></i></a>
+              <a onClick = {() => props.whenProjClicked(props.id)}className="details-link" title="More Details"><i className="bx bx-link"></i></a>
+            </div>
+          </div>
+    </React.Fragment>
+  );
+}
 
-//         {/* <ol>
-//           <li>{props.name}</li>
-//           <li>{props.image}</li>
-//           <li>{props.imgDes}</li>
-//           <li>{props.category}</li>
-//           <li>{props.projDate}</li>
-//           <li>{props.githubUrl}</li>
-//           <li>{props.description}</li>
-//         </ol> */}
-//       </div>
-//     </React.Fragment>
-//   );
-// }
+Project.propTypes = {
+  name: PropTypes.string,
+  image: PropTypes.string,
+  imgDes: PropTypes.string,
+  type: PropTypes.string,
+  category: PropTypes.string,
+  projDate: PropTypes.string,
+  githubUrl: PropTypes.string,
+  description: PropTypes.string,
+};
 
-// Project.propTypes = {
-//   name: PropTypes.string,
-//   image: PropTypes.string,
-//   imgDes: PropTypes.string,
-//   category: PropTypes.string,
-//   projDate: PropTypes.string,
-//   githubUrl: PropTypes.string,
-//   description: PropTypes.string,
-// };
-
-// export default Project;
+export default Project;

c49747eec36c03ceb4407f5703646feb5cfbfa34
Karlson Drendel <kdrendel99@gmail.com>
2021-09-20 01:25:02 -0700

    Fix reducer bug. Dropdown menu not working.

--- src/reducers/selected-project-reducer.js
+++ src/reducers/selected-project-reducer.js
@@ -1,23 +1,23 @@
-import * as c from './../actions/ActionTypes';
+// import * as c from './../actions/ActionTypes';
 
-// const initialState = {
-//   selectedProj: null
-// }
+// // const initialState = {
+// //   selectedProj: null
+// // }
 
-const projectReducer = (state = 0, action) => {
-  const { id } = action;
-  switch (action.type){
-    case c.ADD_SELECTED_PROJECT:
-      return Object.assign({}, state, {
-        selectedProj: id
-      });
-    case c.REMOVE_SELECTED_PROJECT:
-      return Object.assign({}, state, {
-        selectedProj: 0
-      });
-    default:
-      return state;
-  }
-};
+// const projectReducer = (state = 0, action) => {
+//   const { id } = action;
+//   switch (action.type){
+//     case c.ADD_SELECTED_PROJECT:
+//       return Object.assign({}, state, {
+//         selectedProj: id
+//       });
+//     case c.REMOVE_SELECTED_PROJECT:
+//       return Object.assign({}, state, {
+//         selectedProj: 0
+//       });
+//     default:
+//       return state;
+//   }
+// };
 
-export default projectReducer;
+// export default projectReducer;

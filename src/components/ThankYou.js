cb68bdd2f217ddd23a6426a798a579885ed4b6d2
Karlson Drendel <kdrendel99@gmail.com>
2021-09-22 19:53:21 -0700

    Add email functionality in contact form. Note: using third party for the time being, so the state change doesn't work yet.

--- src/components/ThankYou.js
+++ src/components/ThankYou.js
@@ -1,9 +1,8 @@
 import React from "react";
 
-function Contact(){
+function ThankYou(props){
   return (
     <React.Fragment>
-    <main id="main">
       <div id="about" className="paddsection">
         <div className="container">
           <div className="row justify-content-between">
@@ -19,15 +18,14 @@ function Contact(){
             <div className="col-lg-7">
               <div className="about-descr">
 
-                <p className="p-heading">THANK YOU! </p>
-                <p className="separator">My name is Karlson Drendel, and I'm a passionate Software Engineer/Full-Stack Developer seeking to kickstart my career as a programmer by gaining industry experience. I'm a recent coding bootcamp graduate looking to learn and grow my skillset in a dynamic work environment. I possess thousands of hours of hands-on, full time experience building and testing web apps, databases, and general full-stack development. I'm a motivated learner, and am always excited to learn new technologies. I also enjoy puzzles, creative thinking, photography, writing music, and bouldering.</p>
+                <p className="p-heading">THANK YOU</p>
+                <p className="separator">Thanks for inquiring. I'll be in touch soon!</p>
+                  </div>
               </div>
             </div>
           </div>
-        </div>
-      </main>
     </React.Fragment>
     );
   }
   
-  export default Contact;
+  export default ThankYou;
